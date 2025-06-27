96c6fbff954608bf6aec1f63f1fc36ec445162ad
Arjun <pkillarjun@protonmail.com>
2025-06-27 22:57:34 +0530

    common: add threading and multiprocessing log function
    Signed-off-by: Arjun <pkillarjun@protonmail.com>

--- common/utility.py
+++ common/utility.py
@@ -1,4 +1,6 @@
 import sys
+import threading
+import multiprocessing
 from pathlib import Path
 from colorama import init, Fore, Style
 from datetime import datetime, timedelta
@@ -8,6 +10,9 @@ from common.config import *
 
 init(autoreset=True)
 
+print_lock_t = threading.Lock()
+print_lock_p = multiprocessing.Lock()
+
 
 def parse_run_time(time_str):
     return datetime.strptime(time_str, "%Y%m%dT%H%MZ")
@@ -15,6 +20,16 @@ def parse_run_time(time_str):
 
 def print_log(message, color=Fore.BLUE):
     print(f"{color}{message}{Style.RESET_ALL}")
+
+
+def print_log_t(message, color=Fore.BLUE):
+    with print_lock_t:
+        print(f"{color}{message}{Style.RESET_ALL}")
+
+
+def print_log_p(message, color=Fore.BLUE):
+    with print_lock_p:
+        print(f"{color}{message}{Style.RESET_ALL}")
 
 
 def generate_runs():
